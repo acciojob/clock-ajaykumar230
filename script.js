9ebec68761a0d1de31e7cfff135d64c357cefca4
Ajay kumar <125947653+ajaykumar230@users.noreply.github.com>
2023-06-26 10:00:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+//your JS code here. If requir
+// script.js
+
+function updateTimer() {
+  var timerElement = document.getElementById('timer');
+
+  // Get the current date and time
+  var currentDate = new Date();
+
+  // Format the date and time
+  var date = currentDate.toLocaleDateString();
+  var time = currentDate.toLocaleTimeString();
+
+  // Update the timer element
+  timerElement.textContent = 'Date: ' + date + ' | Time: ' + time;
+}
+
+// Update the timer every second
+setInterval(updateTimer, 1000);
+
+	
+}
